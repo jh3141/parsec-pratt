28ee9ff99c4ce99ec88f06927fcd4b3b44ef03eb
jh3141 <jh314159@googlemail.com>
2016-02-23 17:59:48 +0000

    Add a "mixfix" expression (not in a tidy way)

--- src/Main.hs
+++ src/Main.hs
@@ -17,7 +17,8 @@ import qualified Text.PrettyPrint as PP
 data Expr =
     BinOp Expr String Expr |
     PrefixOp String Expr |
-    IntValue Integer 
+    IntValue Integer |
+    IfThenElse Expr Expr Expr
     deriving Show
     
 data OperatorPrecedence = LAssoc Int | RAssoc Int
@@ -34,6 +35,11 @@ pPrint :: Expr -> PP.Doc
 pPrint (BinOp l op r) =  PP.vcat [PP.text $ "(BinOp " ++ op,
                       PP.nest 4 $ pPrint l,
                       PP.nest 4 $ pPrint r, 
+                      PP.text ")"]
+pPrint (IfThenElse c t f) =  PP.vcat [PP.text $ "(If",
+					  PP.nest 4 $ pPrint c,
+                      PP.nest 4 $ pPrint t,
+                      PP.nest 4 $ pPrint f, 
                       PP.text ")"]
 pPrint x = PP.text $ show x
 
@@ -56,24 +62,25 @@ parseIntValue = wsopt (many1 digit) >>= \ x -> return (IntValue (read x))
 
 -- parse an operator
 operator :: StringParser String
-operator = many1 (oneOf "<>:@~\\/|!£$%^&*-_=+")
+operator = try (string "ifTrue") <|> many1 (oneOf "<>:@~\\/|!£$%^&*-_=+")
 
 -- operator precedence values
 operatorPrecedence :: String -> OperatorPrecedence
-operatorPrecedence "-" = LAssoc 5
-operatorPrecedence "+" = LAssoc 5
-operatorPrecedence "|" = LAssoc 4
-operatorPrecedence "*" = LAssoc 7
-operatorPrecedence "&" = LAssoc 6
-operatorPrecedence "/" = LAssoc 7
-operatorPrecedence "<" = LAssoc 3
-operatorPrecedence ">" = LAssoc 3
-operatorPrecedence "<=" = LAssoc 3
-operatorPrecedence ">=" = LAssoc 3
-operatorPrecedence "||" = LAssoc 1
-operatorPrecedence "&&" = LAssoc 2
-operatorPrecedence "^" = RAssoc 9
-operatorPrecedence _   = LAssoc 4
+operatorPrecedence "-" = LAssoc 50
+operatorPrecedence "+" = LAssoc 50
+operatorPrecedence "|" = LAssoc 40
+operatorPrecedence "*" = LAssoc 70
+operatorPrecedence "&" = LAssoc 60
+operatorPrecedence "/" = LAssoc 70
+operatorPrecedence "<" = LAssoc 30
+operatorPrecedence ">" = LAssoc 30
+operatorPrecedence "<=" = LAssoc 30
+operatorPrecedence ">=" = LAssoc 30
+operatorPrecedence "||" = LAssoc 10
+operatorPrecedence "&&" = LAssoc 20
+operatorPrecedence "^" = RAssoc 90
+operatorPrecedence "ifTrue" = RAssoc 5
+operatorPrecedence _   = LAssoc 40
 
 -- parse an operator only if the next operator has at least minimum precedence
 -- (will usually be used with 'try', so error message caused on failure should 
@@ -102,16 +109,27 @@ parseBracketExpr = between openBracket closeBracket parseExpr
     where openBracket = wsopt (char '(')
           closeBracket = wsopt (char ')')
 
+-- parse an if-expression, with syntax cond "ifTrue" true-expr "else" false-expr
+parseIf :: Expr -> ExprParser
+parseIf condition = do
+	trueExpr <- parseExprWithMinimumPrecedence ifPrecedence
+	string "else"
+	falseExpr <- parseExprWithMinimumPrecedence ifPrecedence
+	return $ IfThenElse condition trueExpr falseExpr
+	where
+		ifPrecedence = operatorPrecedence "ifTrue"
+	
 -- given an already parsed expression, parse <operator> <expression> that may 
 -- optionally follow it
 parseInfix :: Int -> Expr -> ExprParser
 parseInfix precedence lhs = do
     maybeOp <- optionMaybe (try (operatorWithMinimumPrecedence precedence))
     case maybeOp of
-        Just op   -> do
+    	Just "ifTrue"  -> parseIf lhs
+        Just op         -> do
         	rhs <- parseExprWithMinimumPrecedence (operatorPrecedence op)
         	parseInfix precedence (BinOp lhs op rhs)
-        Nothing   -> return lhs
+        Nothing         -> return lhs
 
 -- parse terms
 parseTerm :: ExprParser
